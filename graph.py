a29aeb0ec3e58eaccead13594f16d1a7fab99510
pranav <pranavs251206@gmail.com>
2025-11-28 18:36:26 +0530

    Add files via upload

---
+++ graph.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+students=['pranav','sakshi','rohan','abbas','satvik','ishant']
+marks=[92,89,85,91,90,91]
+plt.plot(students,marks)
+plt.xlabel("students")
+plt.ylabel("marks")
+plt.title("the graph of the marks vs students")
+plt.show()
