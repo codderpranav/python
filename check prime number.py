2405db7008a44020d70ee6ff7b82fc203b3c3995
pranav <pranavs251206@gmail.com>
2025-12-06 14:38:45 +0530

    Add files via upload

---
+++ check prime number.py
@@ -0,0 +1,13 @@
+n=int(input("enter the number:"))
+if n>1:
+    for i in range(2,n):
+        if n%i==0:
+            print("the number is not prime")
+            break
+    else:
+        print("the number is prime")
+            
+
+else:
+    print("the number is not prime")            
+
