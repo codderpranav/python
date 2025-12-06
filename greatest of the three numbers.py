0409b664e048964d575e19c692f660f64a971307
pranav <pranavs251206@gmail.com>
2025-12-06 14:45:20 +0530

    Add files via upload

---
+++ greatest of the three numbers.py
@@ -0,0 +1,9 @@
+def number(a,b,c):
+    if a>b and a>c:
+        print("the a is greater number")
+    elif b>a and b>c:
+        print("the number b is greater")
+    else:
+        print("the number c is greater")
+number(25,67,89)
+number(10,13,12)
