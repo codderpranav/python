3611375b7d48a4055abf874503258c2b2aee3e72
pranav <pranavs251206@gmail.com>
2025-12-07 19:22:08 +0530

    Add files via upload

---
+++ file.py
@@ -0,0 +1,7 @@
+file=open("pranav.txt","w")
+#file.write("hiii my self pranav shinde\n ")
+#file.write("i am studying in amity institution of the technology\n ")
+#file.write("my aim is to become the cyber security expert\n")
+file.writelines(["hiii my self pranav satish shinde\n",
+"i am studying in the amity institute of the information technology\n",
+"my aim is to become the data scientiest "])
